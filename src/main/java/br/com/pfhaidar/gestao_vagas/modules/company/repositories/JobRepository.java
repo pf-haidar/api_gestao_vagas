72d218300c2bd2105bf1583745de2420a97d2472
pf-haidar <pedrohaidarfilho@gmail.com>
2023-11-14 16:05:29 -0300

    feat company & job controllers

---
+++ src/main/java/br/com/pfhaidar/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,9 @@
+package br.com.pfhaidar.gestao_vagas.modules.company.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.pfhaidar.gestao_vagas.modules.company.entities.JobEntity;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {}
