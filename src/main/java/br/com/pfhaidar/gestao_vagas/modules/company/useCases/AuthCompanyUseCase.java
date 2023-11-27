31875ba7b940fa1e6f616105ef6cf1f27e1ccc9e
pf-haidar <pedrohaidarfilho@gmail.com>
2023-11-27 16:22:31 -0300

    feat company JWT Token & fix job entities

--- src/main/java/br/com/pfhaidar/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/pfhaidar/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.pfhaidar.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +44,9 @@ public class AuthCompanyUseCase {
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
+        // Configuracao de Token
         var token = JWT.create().withIssuer("javagas")
+            .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
             .withSubject(company.getId().toString())
             .sign(algorithm);
         return token;
